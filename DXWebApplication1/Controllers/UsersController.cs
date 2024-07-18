35cebb26cb4322e609755fd75a8723f42c7a3164
Roberta Okada <robertaokada144@gmail.com>
2024-07-18 14:48:24 -0300

    implementação do services de users

--- DXWebApplication1/Controllers/UsersController.cs
+++ DXWebApplication1/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
 ﻿using DevExpress.Web.Mvc;
+using DXWebApplication1.Models.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,31 +10,29 @@ namespace DXWebApplication1.Controllers
 {
     public class UsersController : Controller
     {
-        // GET: Users
         public ActionResult Index()
         {
             return View();
         }
 
-        DXWebApplication1.Models.DbAp.Model1 db = new DXWebApplication1.Models.DbAp.Model1();
-
         [ValidateInput(false)]
         public ActionResult GridViewPartial()
         {
-            var model = db.users;
-            return PartialView("~/Views/Users/_GridViewPartial.cshtml", model.ToList());
+
+            var model = UsersService.GetUsers();
+
+
+            return PartialView("~/Views/Users/_GridViewPartial.cshtml", model);
         }
 
         [HttpPost, ValidateInput(false)]
         public ActionResult GridViewPartialAddNew(DXWebApplication1.Models.DbAp.user item)
         {
-            var model = db.users;
             if (ModelState.IsValid)
             {
                 try
                 {
-                    model.Add(item);
-                    db.SaveChanges();
+                    UsersService.AddUsers(item);
                 }
                 catch (Exception e)
                 {
@@ -42,22 +41,17 @@ namespace DXWebApplication1.Controllers
             }
             else
                 ViewData["EditError"] = "Please, correct all errors.";
-            return PartialView("~/Views/Users/_GridViewPartial.cshtml", model.ToList());
+            var model = UsersService.GetUsers();
+            return PartialView("~/Views/Users/_GridViewPartial.cshtml", model);
         }
         [HttpPost, ValidateInput(false)]
         public ActionResult GridViewPartialUpdate(DXWebApplication1.Models.DbAp.user item)
         {
-            var model = db.users;
             if (ModelState.IsValid)
             {
                 try
                 {
-                    var modelItem = model.FirstOrDefault(it => it.userId == item.userId);
-                    if (modelItem != null)
-                    {
-                        this.UpdateModel(modelItem);
-                        db.SaveChanges();
-                    }
+                    UsersService.UpdateUsers(item);
                 }
                 catch (Exception e)
                 {
@@ -66,29 +60,29 @@ namespace DXWebApplication1.Controllers
             }
             else
                 ViewData["EditError"] = "Please, correct all errors.";
-            return PartialView("~/Views/Users/_GridViewPartial.cshtml", model.ToList());
+
+            var model = UsersService.GetUsers();
+
+            return PartialView("~/Views/Users/_GridViewPartial.cshtml", model);
         }
         [HttpPost, ValidateInput(false)]
         public ActionResult GridViewPartialDelete(System.Int32 userId)
         {
-            var model = db.users;
             if (userId >= 0)
             {
                 try
                 {
-                    var item = model.FirstOrDefault(it => it.userId == userId);
-                    if (item != null)
-                        model.Remove(item);
-                    db.SaveChanges();
+                    UsersService.DeleteUsers(userId);
                 }
                 catch (Exception e)
                 {
                     ViewData["EditError"] = e.Message;
                 }
             }
+            var model = UsersService.GetUsers();
             return PartialView("~/Views/Users/_GridViewPartial.cshtml", model.ToList());
         }
 
-        
+
     }
 }
---
+++ DXWebApplication1/Models/Services/UsersService.cs
@@ -0,0 +1,69 @@
+﻿using DXWebApplication1.Models.DbAp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace DXWebApplication1.Models.Services
+{
+    public static class UsersService
+    {
+        private static DXWebApplication1.Models.DbAp.Model1 db = new Model1();
+        public static List<user> GetUsers()
+        {
+            var model = db.users.ToList();
+            if (model == null)
+            {
+                throw new Exception("Algo deu errado");
+            }
+            return model;
+        }
+
+        public static void AddUsers(user newUser)
+        {
+            if(!(newUser is user))
+            {
+                throw new Exception();
+            }
+
+            db.users.Add(newUser);
+            db.SaveChanges();
+        }
+
+        public static void UpdateUsers(user updatedUser)
+        {
+            if (!(updatedUser is user))
+            {
+                throw new Exception();
+            }
+
+            var model = db.users.ToList();
+            var modelItem = db.users.FirstOrDefault(x => x.userId == updatedUser.userId);
+            if (modelItem != null)
+            {
+                modelItem.role = updatedUser.role;
+                modelItem.name = updatedUser.name;
+                modelItem.age = updatedUser.age;
+                modelItem.bio = updatedUser.bio;
+                modelItem.createdAt = updatedUser.createdAt;
+                db.SaveChanges();
+            }
+        }
+
+        public static void DeleteUsers(int id)
+        {
+            var model = db.users.ToList();
+            var modelItem = db.users.FirstOrDefault(x => x.userId == id);
+            if(modelItem != null)
+            {
+
+                db.users.Remove(modelItem);
+                db.SaveChanges();
+            } else
+            {
+                throw new Exception("User não encontrado");
+            }
+        }
+    }
+}

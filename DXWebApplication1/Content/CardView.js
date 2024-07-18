25fc3e8891b84985a12cab6499fef38544386002
Roberta Okada <robertaokada144@gmail.com>
2024-07-18 19:37:21 -0300

    Menu para cardview e gridview

---
+++ DXWebApplication1/Content/CardView.js
@@ -0,0 +1,36 @@
+﻿function onPageToolbarItemClick(s, e) {
+    switch (e.item.name) {
+        case "New":
+            CardView.AddNewCard();
+            break;
+        case "Edit":
+            CardView.StartEditCard(CardView.GetFocusedCardIndex());
+            break;
+        case "Delete":
+            console.log("socorro");
+            deleteSelectedRecords();
+            break;
+        case "Export":
+            CardView.ExportTo(ASPxClientCardViewExportFormat.Xlsx);
+            break;
+    }
+}
+function deleteSelectedRecords() {
+    console.log("aaaaa");
+    if (confirm('Confirm Delete?')) {
+        var id = CardView.GetFocusedCardIndex();
+        console.log(id);
+        var p = CardView.GetCardKey(id);
+        console.log(p);
+        CardView.DeleteCardByKey(p);
+
+    }
+}
+
+function onFilterPanelExpanded() {
+    console.log("Entrou");
+}
+
+function adjustPageControls() {
+    console.log("Entrou no panel");
+}
--- DXWebApplication1/Content/GridView.js
+++ DXWebApplication1/Content/GridView.js
@@ -1,27 +1,5 @@
-﻿(function() {
-    var selectedIds;
-    function onGridViewInit(s, e) {
-        AddAdjustmentDelegate(adjustGridView);
-        updateToolbarButtonsState();
-    }
-    function onGridViewSelectionChanged(s, e) {
-        updateToolbarButtonsState();
-    }
-    function adjustGridView() {
-        gridView.AdjustControl();
-    }
-    function updateToolbarButtonsState() {
-        var enabled = gridView.GetSelectedRowCount() > 0;
-        pageToolbar.GetItemByName("Delete").SetEnabled(enabled);
-        pageToolbar.GetItemByName("Export").SetEnabled(enabled);
-
-        pageToolbar.GetItemByName("Edit").SetEnabled(gridView.GetFocusedRowIndex() !== -1);
-    }
-    function onPageToolbarItemClick(s, e) {
-        switch(e.item.name) {
-            case "ToggleFilterPanel":
-                toggleFilterPanel();
-                break;
+﻿function onPageToolbarItemClick(s, e) {
+        switch (e.item.name) {
             case "New":
                 gridView.AddNewRow();
                 break;
@@ -29,6 +7,7 @@
                 gridView.StartEditRow(gridView.GetFocusedRowIndex());
                 break;
             case "Delete":
+                console.log("socorro");
                 deleteSelectedRecords();
                 break;
             case "Export":
@@ -37,43 +16,21 @@
         }
     }
     function deleteSelectedRecords() {
-        if(confirm('Confirm Delete?')) {
-            gridView.GetSelectedFieldValues("Id", getSelectedFieldValuesCallback);
+        console.log("aaaaa");
+        if (confirm('Confirm Delete?')) {
+            var id = gridView.GetFocusedRowIndex();
+            console.log(id);
+            var p = gridView.GetRowKey(id);
+            console.log(p);
+            gridView.DeleteRowByKey(p);
+
         }
-    }
-    function onFiltersNavBarItemClick(s, e) {
-        var filters = {
-            All: "",
-            Active: "[Status] = 1",
-            Bugs: "[Kind] = 1",
-            Suggestions: "[Kind] = 2",
-            HighPriority: "[Priority] = 1"
-        };
-        gridView.ApplyFilter(filters[e.item.name]);
-        HideLeftPanelIfRequired();
-    }
+}
 
-    function toggleFilterPanel() {
-        filterPanel.Toggle();
-    }
+function onFilterPanelExpanded() {
+    console.log("Entrou");
+}
 
-    function onFilterPanelExpanded(s, e) {
-        adjustPageControls();
-        searchButtonEdit.SetFocus();
-    }
-
-    function onGridViewBeginCallback(s, e) {
-        e.customArgs['SelectedRows'] = selectedIds;
-    }
-    function getSelectedFieldValuesCallback(values) {
-        selectedIds = values.join(',');
-        gridView.PerformCallback({ customAction: 'delete' });
-    }
-
-    window.onGridViewBeginCallback = onGridViewBeginCallback;
-    window.onGridViewInit = onGridViewInit;
-    window.onGridViewSelectionChanged = onGridViewSelectionChanged;
-    window.onPageToolbarItemClick = onPageToolbarItemClick;
-    window.onFilterPanelExpanded = onFilterPanelExpanded;
-    window.onFiltersNavBarItemClick = onFiltersNavBarItemClick;
-})();
+function adjustPageControls() {
+    console.log("Entrou no panel");
+}
